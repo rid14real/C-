4c699332892bd28702ed327de30e41aab0e44a96
Ridwan Badmus <62150969+rid14real@users.noreply.github.com>
2021-02-12 04:19:52 +0100

    working with array length

---
+++ working with array length.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace ArrayLength
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] summerStrut;
+
+      summerStrut = new string[] { "Juice", "Missing U", "Raspberry Beret", "New York Groove", "Make Me Feel", "Rebel Rebel", "Despacito"};
+    if (summerStrut.Length == 8)
+    {
+    Console.WriteLine("summerStrut Playlist is ready to go!");
+    }
+    else if (summerStrut.Length > 8)
+    {
+      Console.WriteLine("Too many songs!");
+    }
+    else
+    {
+      Console.WriteLine("Add some songs!");
+    }
+    }
+  }
+}
